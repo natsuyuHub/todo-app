626ef5f33ee93100f4cf44e1ad9e0e09c8eaf1d1
natsuyuHub <yu.fujitsuna@gmail.com>
2022-01-13 06:07:20 +0900

    change: Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,16 +25,35 @@ module.exports = {
 		'prettier'
 	],
 	rules: {
+		'no-undef':0,
+		'no-console':0,
+		'func-names':0,
+		'consistent-return':0,
 		'react/prop-types': 0,
+		'no-unused-expressions':0,
 		'react/react-in-jsx-scope': 0,
+		'react/jsx-filename-extension':0,
+		'react/destructuring-assignment':0,
 		'react/display-name': 0,
 		'no-extra-semi': 0,
 		'import/no-unresolved': 0,
+		'import/extensions': 0,
+    'import/no-duplicates': 0,
+		'import/no-extraneous-dependencies':0,
 		'@typescript-eslint/semi': ['error', 'never'],
 		'@typescript-eslint/no-extra-semi': ['error'],
 		'@typescript-eslint/no-explicit-any': 0,
 		'jsx-a11y/no-autofocus': 0,
+		'jsx-a11y/label-has-associated-control':0,
 		'jsx-a11y/click-events-have-key-events': 0,
 		'jsx-a11y/no-static-element-interactions': 0
-	}
+	},
+	overrides: [
+    {
+      files: ['index.css'],
+      rules: {
+        'parserOptions': 'off'
+      },
+    },
+  ],
 }
